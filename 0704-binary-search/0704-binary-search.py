91b99e3f57dd786e23970e9252b5ba53fffdb0bd
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-01-06 22:56:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (65.24%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def search(self, nums, target):
+        first=0
+        last=len(nums)-1
+        while first<=last:
+            mid=(first+last)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                first=mid+1
+            else:
+                last=mid-1
+        return -1
