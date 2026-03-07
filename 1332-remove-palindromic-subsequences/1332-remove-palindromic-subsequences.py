839f0aaae621a4a0941e243dca1428f7e7345268
Mohd Zaheeruddin <info.zaheerjk@gmail.com>
2026-03-07 23:43:12 +0530

    Time: 0 ms (100%), Space: 12.4 MB (60.4%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removePalindromeSub(self, s):
+        i=0
+        j=len(s)-1
+        while i<j:
+            if s[i]!=s[j]:
+                return 2
+            i+=1
+            j-=1
+        return 1
+        
