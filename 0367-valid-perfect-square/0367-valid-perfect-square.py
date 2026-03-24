30f08bb9adf35e5e2caf1b3b6864084c0a72d2df
Mohd Zaheeruddin <info.zaheerjk@gmail.com>
2026-03-24 22:26:44 +0530

    Time: 0 ms (100%), Space: 12.4 MB (40.66%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.py
+++ 0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -5,13 +5,12 @@ class Solution(object):
         first=0
         last=num//2
         while first<=last:
-            mid=(first+last)//2
+            mid=first+(last-first)//2
             square=mid*mid
-            if num==square:
+            if square==num:
                 return True
             elif square>num:
                 last=mid-1
             else:
                 first=mid+1
         return False
-        
