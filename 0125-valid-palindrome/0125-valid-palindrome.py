b2df8dc30c33536cbc136b0f6aa5abfa4b2624a7
Mohd Zaheeruddin <183212672+mdzaheerjk@users.noreply.github.com>
2026-07-05 19:55:09 +0530

    Time: 17 ms (55.26%), Space: 12.5 MB (99.7%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        i=0
+        j=len(s)-1
+        while i<j:
+            if not s[i].isalnum():
+                i+=1
+            elif not s[j].isalnum():
+                j-=1
+            elif s[i].lower()==s[j].lower():
+                i+=1
+                j-=1
+            else:
+                return False
+        return True
